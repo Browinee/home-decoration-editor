e80ee1f593a53cfea3c15737d6ceed49568170a1
Browinee <nihilitypeo@gmail.com>
2025-07-21 17:09:52 +0800

    feat: add init three js

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useRef } from "react";
+import { init3D } from "./init-3d";
+
 function Main() {
-  return <div className="Main">Main</div>
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!containerRef.current) return;
+
+        const { scene } = init3D(containerRef.current);
+
+        return () => {
+            if (containerRef.current) {
+                containerRef.current.innerHTML = '';
+            }
+        }
+    }, []);
+
+    return <div className="Main">
+         <div ref={containerRef}></div>
+    </div>
 }
 
 export default Main;
