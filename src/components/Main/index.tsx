eaa9bc8541485e4f6618c9dfc8b63f2b174cfa0c
Browinee <nihilitypeo@gmail.com>
2025-07-21 17:23:04 +0800

    feat: add 2d/3d switch button

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,56 @@
-import { useThreeRenderer } from "./useThreeRenderer";
+import { useEffect, useRef, useState } from "react";
 import { init3D } from "./init-3d";
 import { init2D } from "./init-2d";
+import { Button } from "antd";
 
 function Main() {
-    const { containerRef: container3DRef } = useThreeRenderer(init3D);
-    const { containerRef: container2DRef } = useThreeRenderer(init2D);
+  const container3DRef = useRef<HTMLDivElement>(null);
+  const container2DRef = useRef<HTMLDivElement>(null);
+  const [curMode, setCurMode] = useState("2d");
+  useEffect(() => {
+    if (!container3DRef.current) return;
 
-    return (
-        <div className="Main">
-            <div ref={container3DRef} id="threejs-3d-container"></div>
-            <div ref={container2DRef} id="threejs-2d-container"></div>
-        </div>
-    );
+    const { scene } = init3D(container3DRef.current);
+
+    return () => {
+      if (container3DRef.current) {
+        container3DRef.current.innerHTML = "";
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!container2DRef.current) return;
+
+    const { scene } = init2D(container2DRef.current);
+
+    return () => {
+      if (container2DRef.current) {
+        container2DRef.current.innerHTML = "";
+      }
+    };
+  }, []);
+
+  return (
+    <div className="Main">
+      <div ref={container3DRef} id="threejs-3d-container" style={{display: curMode ==='3d' ? 'block': 'none'}}></div>
+      <div ref={container2DRef} id="threejs-2d-container" style={{display: curMode ==='2d' ? 'block': 'none'}}></div>
+      <div className="mode-change-btns">
+        <Button
+          type={curMode === "2d" ? "primary" : "default"}
+          onClick={() => setCurMode("2d")}
+        >
+          2D
+        </Button>
+        <Button
+          type={curMode === "3d" ? "primary" : "default"}
+          onClick={() => setCurMode("3d")}
+        >
+          3D
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
